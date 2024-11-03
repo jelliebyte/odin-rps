9ff10d20773eb7eb673357bd07ca24cfdfc3e804
jelliebyte <spooystuff4@gmail.com>
2024-11-02 21:59:24 -0500

    Added prompt for players choice

--- script.js
+++ script.js
@@ -8,16 +8,39 @@ let getComputerChoice = () => {
     case 2:
         return "Scissors";
     default:
-        alert("No number entered!");
+        alert("Invalid!!");
         break;
     }
 }
 
-alert(getComputerChoice());
+let getHumanChoice = () => {
+    choice = prompt("Rock, Paper, Scissors!").toLowerCase();
+
+    if (choice === "rock" || choice === "paper" || choice === "scissors"){
+        return choice;
+    }
+    else {
+        alert("Invalid choice!");
+        location.reload();
+    }
+}
+
+console.log(getHumanChoice());
+console.log(getComputerChoice());
 
 /*
  @todo: make it so that getComputerChoice returns either
  rock, paper, or scissors
 - Make sure it returns 1, 2 or 3
  - Make it return a string
+ */
+
+ /*
+ @todo: 
+create a function for player input
+create a variable to store the players input
+compare this input to "Rock", "Paper", or "Scissors"
+if the comparison is true, return the choice
+if the comparison is NOT true, prompt again
+
  */
