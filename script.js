0cf16e1160f74d0c03c63f28ef9981c4b4951c92
jelliebyte <spooystuff4@gmail.com>
2024-12-06 11:04:56 -0600

    Scores are now displayed correctly

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let humanScore = 0;
+let humanScore = 5;
 let computerScore = 0;
 let choice = "";
 let rounds = 1
@@ -7,38 +7,24 @@ const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorButton = document.querySelector("#scissors");
 const buttonsDiv = document.querySelector("#buttons");
+const hScoreCounter = document.querySelector("#humanScore");
+const cScoreCounter = document.querySelector("#compScore");
+const winLose = document.createElement("div");
 
-let playerChoice = buttonsDiv.addEventListener("click", (event) => {
-    let target = event.target;
 
-    switch (target.id){
-        case 'rock':
-            choice = "rock";
-            return choice;
-        case 'paper':
-            choice = "paper";
-            return choice;
-        case 'scissors':
-            choice = "scissors";
-            return choice;
-        case _:
-            console.log("Invalid!");
-            break;
-    }
-});
 
 let getComputerChoice = () => {
-    randNum = Math.floor(Math.random() * 3);
+    randNum = Math.floor(Math.random() * 3); // gets a number between 1 - 3
     switch (randNum){
     case 0:
         console.log("The computer played: rock!");
         return "rock";
     case 1:
         console.log("The computer played: paper!");
-        return "rock";
+        return "paper";
     case 2:
         console.log("The computer played: scissors!");
-        return "rock";
+        return "scissors";
     default:
         alert("Invalid!!");
         break;
@@ -50,45 +36,57 @@ function playRound(pChoice, cChoice){
     let hasLost = false;
     switch (pChoice){
         case "rock":
-            if (cChoice === "paper"){
-                console.log("Lost!");
+            if  (cChoice === "rock"){
+                console.log("Tie!");
+                break;
+            }
+            else if (cChoice === "paper"){
+                console.log("Lose!");
                 hasLost = true;
                 break;
             }
             else if (cChoice === "scissors"){
-                console.log("Won!");
+                console.log("Win!");
                 hasWon = true;
                 break;
             }
+
         case "paper":
             if (cChoice === "rock"){
-                console.log("Won!");
+                console.log("Win!");
+                hasWon = true;
+                break;
+            }
+            else if (cChoice === "paper"){
+                console.log("Tie!");
+                break;
+            }
+            else if (cChoice === "scissors"){
+                console.log("Lose!");
+                hasLost = true;
+                break;
+            }
+
+        case "scissors":
+            if (cChoice === "rock"){
+                console.log("Lose!");
+                hasLost = true;
+                break;
+            }
+            else if (cChoice === "paper"){
+                console.log("Win!");
                 hasWon = true;
                 break;
             }
             else if (cChoice === "scissors"){
-                console.log("Lost!");
-                hasWon = false;
+                console.log("Tie!");
                 break;
             }
-        case "scissors":
-            if (cChoice === "paper"){
-                console.log("Won!");
-                hasWon = true;
-                break;
-            }
-            else if (cChoice === rock){
-                console.log("Lost!");
-                hasLost = true;
-                break;
-            }
-        case (pChoice === cChoice):
-            console.log("Draw!");
-            break;
         default:
             alert("Nah");
             break;
     }
+    
     if (hasWon === true){
         ++humanScore;
     }
@@ -100,7 +98,16 @@ function playRound(pChoice, cChoice){
         computerScore;
     }
 
-    console.log(`Human score ${humanScore}`);
-    console.log(`Computer score ${computerScore}`);
+    hScoreCounter.textContent= `Human score ${humanScore}`;
+    cScoreCounter.textContent = `Computer score ${computerScore}`;
 }
 
+if (humanScore || computerScore < 5){
+    buttonsDiv.addEventListener("click", (event) => {
+        let target = event.target;
+        playRound(target.id, getComputerChoice());
+    });
+}
+if (humanScore > computerScore){
+    console.log("Done!");
+}
